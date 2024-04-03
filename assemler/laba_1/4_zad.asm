3d80d7c3f0b2fd097206c5f6e37113d0609875a2
Nefedov George <nefedov.goga@yandex.ru>
2024-04-03 12:55:24 +0300

    fix_1

--- assemler/laba_1/4_zad.asm
+++ assemler/laba_1/4_zad.asm
@@ -1,63 +1,15 @@
- 	section .data ; сегмент инициализированных переменных
-ExitMsg db "Press Enter to Exit",10 ; выводимое сообщение
-lenExit equ $-ExitMsg
+section .data
+    f1 dw 65535 ; 0xffff
+    f2 dd 65535 ; 0x0000ffff
 
-numA dw 25
-num_b dd -35
-nameru db 'Георгий', 0
-name db 'George', 0
-
-num1 db 37
-num1_1 db 0b100101
-
-num2 dw 9472
-num2_2 dw 0o22400
-
-F1 dw 65534
-F2 dd 65535
-
-A dw -30
-B dw 21
-val1 db 255
-chart dw 256
-lue3 dw -128
-v5 db 10h
-db 100101B
-beta db 23,23h,0ch
-sdk db "Hello",10
-min dw -32767
-ar dd 12345678h
-valar times 5 db 8
-	section .bss ; сегмент неинициализированных переменных
-InBuf resb 10 ; буфер для вводимой строки
-lenIn equ $-InBuf
-X resd 1 ; переменная для результата
-alu resw 10
-f1 resb 5
-section .text ; сегмент кода
+section .text
 global _start
-_start:
-; write
-	mov eax, 4 ; системная функция 4 (write)
-	mov ebx, 1 ; дескриптор файла stdout=1
-	mov ecx, ExitMsg ; адрес выводимой строки
-	mov edx, lenExit ; длина выводимой строки
-	int 80h ; вызов системной функции
-	
-	mov eax, [A]
-	add eax, 5 
-	sub eax, [B] 
-	mov [X], eax 
 
-	add [F1],1
-	add [F2],1
-	; read
-	mov eax, 3 ; системная функция 3 (read)
-	mov ebx, 0 ; дескриптор файла stdin=0
-	mov ecx, InBuf ; адрес буфера ввода
-	mov edx, lenIn ; размер буфера
-	int 80h ; вызов системной функции
-				; exit
-	mov eax, 1 ; системная функция 1 (exit)
-	xor ebx, ebx ; код возврата 0
-	int 80h
+_start:
+    mov ax,  [f1]
+    mov ebx, [f2]
+    add ax,  1    ; 0x0000, OF
+    add ebx, 1    ; 0x00010000
+
+    mov eax, 1
+    int 80h
